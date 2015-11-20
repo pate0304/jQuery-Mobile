c6017e36c0416915f9c7162c830face935769103
Jay Patel <pate0304@algonquinlive.com>
2015-11-20 09:34:33 -0500

    :D

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var stepNum;
+$(document).on("mobileinit", function() {});
+$(document).on("ready", function(ev) {
+   $("#startTut").on("click", function(ev) {
+       ev.preventDefault();
+       if (localStorage.getItem("step")) {
+           stepNum = JSON.parse(localStorage.getItem("step"));
+       }
+       var returnPage = "#sun" + stepNum;
+       $.mobile.pageContainer.pagecontainer("change", returnPage);
+   });
+   $(".nextP").on("click", function(ev) {
+       stepNum += 1;
+       localStorage.setItem("step", JSON.stringify(stepNum));
+   });
+   $(".backP").on("click", function(ev) {
+       stepNum -= 1;
+       localStorage.setItem("step", JSON.stringify(stepNum));
+   });
+});
