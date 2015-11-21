5305c05ead8c156ad9bb74f501bf7f8c8d0365a0
Jay Patel <pate0304@algonquinlive.com>
2015-11-20 23:19:34 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-var stepNum;
+var stepNum=1;
 $(document).on("mobileinit", function() {});
 $(document).on("ready", function(ev) {
    $("#startTut").on("click", function(ev) {
        ev.preventDefault();
        if (localStorage.getItem("step")) {
-           stepNum = JSON.parse(localStorage.getItem("step"));
+           stepNum = JSON.parse(localStorage.getItem("step"))
        }
-       var returnPage = "#sun" + stepNum;
+       var returnPage = "#sun" + stepNum.toString();
        $.mobile.pageContainer.pagecontainer("change", returnPage);
    });
    $(".nextP").on("click", function(ev) {
